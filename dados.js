dfcee8b8d95c6a9e4f97dfc85d86a2b5cd35a005
JohnVitor-DEV <johnvitoralves39@gmail.com>
2024-09-09 13:28:39 -0300

    09/09/23

---
+++ dados.js
@@ -0,0 +1,14 @@
+let Projetos = [ 
+    {
+        name: "TamagotshiPokemon",
+        description: "",
+        link: "",
+        language: ""
+    },
+    {
+        name: "test",
+        description: "text",
+        link: "link",
+        language: "JavaScript"
+    }
+];
